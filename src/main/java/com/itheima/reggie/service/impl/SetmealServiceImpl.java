3b3f66ac98ff5d01fec6281860d078700e2f5e38
kunshao <1464227787@qq.com>
2022-10-04 22:05:42 +0800

    添加了套餐的批量删除功能功能

--- src/main/java/com/itheima/reggie/service/impl/SetmealServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/SetmealServiceImpl.java
@@ -1,6 +1,8 @@
 package com.itheima.reggie.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.common.CustomException;
 import com.itheima.reggie.dto.SetmealDto;
 import com.itheima.reggie.entity.Setmeal;
 import com.itheima.reggie.entity.SetmealDish;
@@ -37,6 +39,32 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> impl
         //保存套餐和菜品的关联关系，操作Setmeal_dish表，执行insert操作
             setmealDishService.saveBatch(setmealDishes);
 
+
+    }
+
+    /**
+     * 删除套餐，同时需要删除套餐和菜品的关联数据
+     * @param ids
+     */
+    @Transactional
+    public void removeWithDish(List<Long> ids) {
+        //查询套餐状态，确定是否可以删除
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.in(Setmeal::getId,ids);
+        queryWrapper.eq(Setmeal::getStatus,1);
+        int count = this.count(queryWrapper);
+
+        if (count > 0){
+            //如果不能删除，抛出一个业务异常
+            throw new CustomException("套餐正在售卖中，不能删除");
+        }
+
+        //如果可以删除，先删除套餐中的数据---setmeal
+        this.removeByIds(ids);
+        //删除关系表中的数据---setmeal_dish
+        LambdaQueryWrapper<SetmealDish> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.in(SetmealDish::getSetmealId,ids);
+        setmealDishService.remove(lambdaQueryWrapper);
 
     }
 }
